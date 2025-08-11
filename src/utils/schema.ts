b660e8fc0e736f8184716408689652c06041953a
hustcc <i@hust.cc>
2025-08-11 20:42:46 +0800

    fix: remove  in json schema of generate_district_map (#173)

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -6,5 +6,6 @@ import { zodToJsonSchema as zodToJsonSchemaOriginal } from "zod-to-json-schema";
 export const zodToJsonSchema = (schema: Record<string, z.ZodType<any>>) => {
   return zodToJsonSchemaOriginal(z.object(schema), {
     rejectedAdditionalProperties: undefined,
+    $refStrategy: "none",
   });
 };
